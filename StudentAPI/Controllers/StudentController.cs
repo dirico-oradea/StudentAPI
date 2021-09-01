6088f733dbc36ed2cbe163de395cda3ed98982c5
imre.kelemen.internship <kel634@yahoo.com>
2021-09-01 19:17:29 +0300

    initial commit

---
+++ StudentAPI/Controllers/StudentController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentAPI.Models;
+using StudentAPI.Services;
+
+namespace StudentAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private StudentService studentService = new StudentService();
+
+        [HttpGet]
+        [Route("all")]
+        public ActionResult<List<Student>> GetAllStudents([FromHeader]string password)
+        {
+            if (password != "rightpassword")
+            {
+                return Unauthorized();
+            }
+            return this.studentService.GetAllStudents();
+        }
+
+        [HttpGet]
+        [Route("id/{studentId}")]
+        public ActionResult<Student> GetStudentById([FromRoute]int studentId)
+        {
+            try
+            {
+                return Ok(this.studentService.GetStudentById(studentId));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        [Route("find")]
+        public List<Student> GetStudentsByProfile([FromQuery]string profile)
+        {
+            return this.studentService.GetStudentsByProfile(profile);
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public ActionResult<Student> CreateStudent([FromBody]Student student)
+        {
+            return Created("id/" + student.Id, this.studentService.CreateStudent(student));
+        }
+
+        [HttpDelete]
+        [Route("delete/{studentId}")]
+        public ActionResult DeleteStudentById([FromRoute]int studentId)
+        {
+            try
+            {
+                this.studentService.DeleteStudentById(studentId);
+
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut]
+        [Route("update/{studentId}")]
+        public ActionResult<Student> UpdateStudentById([FromRoute]int studentId, [FromBody]Student changedStudent)
+        {
+            try
+            {
+                return Ok(this.studentService.UpdateStudentById(studentId, changedStudent));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ StudentAPI/Services/StudentService.cs
@@ -0,0 +1,61 @@
+﻿using StudentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAPI.Services
+{
+    public class StudentService
+    {
+        private static List<Student> MyStudents = new List<Student>()
+        {
+            new Student() { Id = 1, Name = "John Doe", Profile = "IT" },
+            new Student() { Id = 2, Name = "Jane Doe", Profile = "IT" },
+            new Student() { Id = 3, Name = "John Smith", Profile = "Physics" },
+        };
+
+        public List<Student> GetAllStudents()
+        {
+            return MyStudents;
+        }
+
+        public Student GetStudentById(int studentId)
+        {
+            return MyStudents.First(s => s.Id == studentId);
+        }
+
+        public List<Student> GetStudentsByProfile(string profile)
+        {
+            return MyStudents.Where(s => s.Profile == profile).ToList();
+        }
+
+        public Student CreateStudent(Student student)
+        {
+            MyStudents.Add(student);
+            return student;
+        }
+
+        public void DeleteStudentById(int studentId)
+        {
+            if (MyStudents.Any(s => s.Id == studentId))
+            {
+                MyStudents = MyStudents.Where(s => s.Id != studentId).ToList();
+            }
+            else
+            {
+                throw new Exception("The student with the specified ID was not found.");
+            }
+        }
+
+        public Student UpdateStudentById(int studentId, Student changedStudent)
+        {
+            Student studentToUpdate = MyStudents.First(s => s.Id == studentId);
+
+            studentToUpdate.Name = changedStudent.Name;
+            studentToUpdate.Profile = changedStudent.Profile;
+
+            return studentToUpdate;
+        }
+    }
+}
